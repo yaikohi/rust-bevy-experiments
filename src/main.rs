335edad9366ad4c0d710bbdd795d681ecfb27682
yaikohi <erik.beem@hotmail.com>
2022-05-26 13:19:16 +0200

    Start bevy book/tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use bevy::prelude::*;
+
+// // entity
+// struct Entity(f64)
+
+// // component
+// #[derive(Component)]
+// struct Position { x: f32, y: f32 }
+
+// // system
+// fn print_position_system(query: Query<&Transform>) {
+//     for transform in query.iter() {
+//         println!("Position: {:?}", transform.translation);
+//     }
+// }
+// ENTITY COMPONENT SYSTEM = ECS
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Wong Kar".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Michael Grooves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people)
+        .add_system(greet_people)
+        .run();
 }
